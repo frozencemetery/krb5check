313f2f2e4227eabd6a3de51fa084a1e60b879579
Robbie Harwood <rharwood@redhat.com>
2020-03-06 13:50:29 -0500

    Handle cross-realm

--- principals.py
+++ principals.py
@@ -53,9 +53,23 @@ def handle_krbtgt(krbtgt: Optional[str]) -> None:
         print("- good encryption types.")
         print("")
 
-def handle_cross_realm() -> None:
-    # TODO
-    return
+def handle_cross_realm(croses: List[str]) -> None:
+    need_updated = []
+    for cross in crosses:
+        good, _ = partition_etypes(cross)
+        if len(good) == 0:
+            need_updated.append(cross)
+
+    if len(need_updated) == 0:
+        return
+
+    print("! These cross-realm relationships rely on weak encryption:")
+    print("!     " + " ".join(need_updated))
+    print("! They need to be recreaed.  This requires coordination with the")
+    print("! administration of those realms.  A new key must be established")
+    print("! over a very secure channel.  For more information, see:")
+    print("! https://web.mit.edu/kerberos/krb5-latest/doc/admin/database.html#cross-realm-authentication")
+    print("")
 
 def handle_km() -> None:
     _, bad_etypes = partition_etypes("K/M")
@@ -74,7 +88,6 @@ def handle_km() -> None:
     print("")
 
 def handle_users(users: List[str]) -> None:
-    # Handle users
     nogood = []
     havebad = []
     for u in users:
@@ -98,7 +111,6 @@ def handle_users(users: List[str]) -> None:
         print("")
 
 def handle_services(services: List[str]) -> None:
-    # Handle services
     nogood = []
     havebad = []
     for s in services:
@@ -133,15 +145,21 @@ if __name__ == "__main__":
 
     del(princs[0])
     krbtgt = None
+    crosses = []
     users = []
     services = []
     for p in princs:
         short, realm = p.rsplit("@", 1)
         if short == "K/M":
+            # Assume we always have the one K/M
             continue
         elif short.startswith("krbtgt/"):
-            # TODO this doesn't handle cross-realm yet
-            krbtgt = p
+            foreign = short[short.index("/") + 1:]
+            if foreign != realm:
+                crosses.append(p)
+            else:
+                krbtgt = p
+
             continue
         elif "/" not in short:
             users.append(p)
@@ -150,7 +168,7 @@ if __name__ == "__main__":
         services.append(p)
 
     handle_krbtgt(krbtgt)
-    handle_cross_realm()
+    handle_cross_realm(crosses)
     handle_km()
     handle_users(users)
     handle_services(services)
