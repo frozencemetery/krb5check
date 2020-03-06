32783df48bc022b20e9e5987978a9ed62c9375be
Robbie Harwood <rharwood@redhat.com>
2020-03-06 12:41:27 -0500

    Split program into functions to emphasize code duplication

--- principals.py
+++ principals.py
@@ -30,41 +30,20 @@ def partition_etypes(princ: str) -> Tuple[List[str], List[str]]:
         bads.append(e)
     return goods, bads
 
-print("Hello!  Remember to back up your KDC before making changes.\n")
-# TODO maybe some info about the stuff we warn about and why?
+def handle_krbtgt() -> None:
+    # TODO
+    return
 
-princs = kl("-q listprincs").split('\n')
-if not princs[0].startswith("Authenticating as principal "):
-    print("Error: couldn't list principals!")
-    exit(-1)
+def handle_cross_realm() -> None:
+    # TODO
+    return
 
-del(princs[0])
+def handle_km() -> None:
+    _, bad_etypes = partition_etypes("K/M")
+    if len(bad_etypes) == 0:
+        print("(K/M looks okay)\n")
+        return
 
-kms = []
-users = []
-services = []
-for p in princs:
-    if p.startswith("K/M@"):
-        kms.append(p)
-        continue
-
-    short, realm = p.rsplit("@", 1)
-    if "/" not in short:
-        users.append(p)
-        continue
-
-    services.append(p)
-
-# TODO Handle krbtgt!
-
-# Handle K/M
-if len(kms) != 1:
-    print("More than one K/M detected.  We're not prepared for that.")
-    print(kms)
-    exit(-1)
-km = kms[0]
-_, bad_etypes = partition_etypes(km)
-if len(bad_etypes) != 0:
     print("! K/M (the database master key) uses bad encryption types:")
     print("!     " + " ".join(bad_etypes))
     print("! Use kdb5_util to fix.  Suggested commands:")
@@ -74,54 +53,83 @@ if len(bad_etypes) != 0:
     print("!     kdb5_util update_princ_encryption # will prompt")
     print("!     kdb5_util purge_mkeys")
     print("")
-else:
-    print("(K/M looks okay)\n")
     
-# Handle users
-nogood = []
-havebad = []
-for u in users:
-    goods, bads = partition_etypes(u)
-    if len(goods) == 0:
-        nogood.append(u)
-    elif len(bads) != 0:
-        havebad.append(u)
+def handle_users(users: List[str]) -> None:
+    # Handle users
+    nogood = []
+    havebad = []
+    for u in users:
+        goods, bads = partition_etypes(u)
+        if len(goods) == 0:
+            nogood.append(u)
+        elif len(bads) != 0:
+            havebad.append(u)
 
-if len(nogood) != 0:
-    print("! The following user principals have no good encryption types:")
-    print("!     " + " ".join(nogood))
-    print("! They need to be rekeyed.  If they're password based, ")
-    print("! this can be accomplished with kpasswd / kadmin change_password")
-    print("")
-if len(havebad) != 0:
-    print("- The following user principals have some bad encryption types:")
-    print("-     " + " ".join(havebad))
-    print("- This is only a problem if your machines do not mandate")
-    print("- good encryption types")
-    print("")
+    if len(nogood) != 0:
+        print("! The following users have no good encryption types:")
+        print("!     " + " ".join(nogood))
+        print("! They need to be rekeyed.  Usually, this can be fixed with ")
+        print("! kpasswd or kadmin change_password")
+        print("")
+    if len(havebad) != 0:
+        print("- The following users have some bad encryption types:")
+        print("-     " + " ".join(havebad))
+        print("- This is only a problem if your machines do not mandate")
+        print("- good encryption types")
+        print("")
 
-# Handle services
-nogood = []
-havebad = []
-for s in services:
-    goods, bads = partition_etypes(s)
-    if len(goods) == 0:
-        nogood.append(s)
-    elif len(bads) != 0:
-        havebad.append(s)
+def handle_services(services: List[str]) -> None:
+    # Handle services
+    nogood = []
+    havebad = []
+    for s in services:
+        goods, bads = partition_etypes(s)
+        if len(goods) == 0:
+            nogood.append(s)
+        elif len(bads) != 0:
+            havebad.append(s)
 
-if len(nogood) != 0:
-    print("! The following service principals have no good encryption types:")
-    print("!     " + " ".join(nogood))
-    print("! They need to be rekeyed, and all keytabs updated.")
-    print("! (Typically it is sufficient to generate new keytabs and copy ")
-    print("! them to the relevant servers.)")
-    print("")
-if len(havebad) != 0:
-    print("- The following service principals have some bad encryption types:")
-    print("-     " + " ".join(havebad))
-    print("- This is only a problem if your machines do not mandate")
-    print("- good encryption types")
-    print("")
+    if len(nogood) != 0:
+        print("! The following services have no good encryption types:")
+        print("!     " + " ".join(nogood))
+        print("! They need to be rekeyed, and all keytabs updated.")
+        print("! (Typically it is sufficient to generate new keytabs and ")
+        print("! move them to the relevant servers.)")
+        print("")
+    if len(havebad) != 0:
+        print("- The following services have some bad encryption types:")
+        print("-     " + " ".join(havebad))
+        print("- This is only a problem if your machines do not mandate")
+        print("- good encryption types")
+        print("")
 
-print("(All done)")
+if __name__ == "__main__":
+    print("Hello!  Remember to back up your KDC before making changes.\n")
+    # TODO maybe some info about the stuff we warn about and why?
+
+    princs = kl("-q listprincs").split('\n')
+    if not princs[0].startswith("Authenticating as principal "):
+        print("Error: couldn't list principals!")
+        exit(-1)
+
+    del(princs[0])
+
+    users = []
+    services = []
+    for p in princs:
+        short, realm = p.rsplit("@", 1)
+        if short == "K/M":
+            continue
+        elif "/" not in short:
+            users.append(p)
+            continue
+
+        services.append(p)
+
+    handle_krbtgt()
+    handle_cross_realm()
+    handle_km()
+    handle_users(users)
+    handle_services(services)
+
+    print("(All done)")
